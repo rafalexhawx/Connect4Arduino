a3372f6bdbbfc3c1481e7bc9e6e8a289ace21d92
SlamaFR <SlamaFR@users.noreply.github.com>
2019-03-29 16:44:28 +0100

    First version of puissance4.ino

---
+++ puissance4.ino
@@ -0,0 +1,71 @@
+const int LATCH = 10;
+const int SHIFT = 11;
+const int RESET = 12;
+
+const int ROW = 7;
+const int RED = 8;
+const int GREEN = 9;
+
+const int MATRIX_SIZE = 8;
+
+const char COLORS_NAMES = {'R', 'G', 'O', 'B'};
+const bool COLORD_BOOLS = {{true,  false},
+                           {false, true},
+                           {true,  true},
+                           {false, false}};
+
+const char grid[8][8] =
+        {
+                {' ', 'R', 'R', ' ', 'R', 'R', ' ', ' '},
+                {'R', 'R', 'R', 'R', 'R', 'R', 'R', ' '},
+                {'R', 'R', 'R', 'R', 'R', 'R', 'R', ' '},
+                {' ', 'R', 'R', 'R', 'R', 'R', ' ', ' '},
+                {' ', ' ', 'R', 'R', 'R', ' ', ' ', ' '},
+                {' ', ' ', ' ', 'R', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+        };
+
+void setup() {
+    pinMode(LATCH, OUTPUT);
+    pinMode(SHIFT, OUTPUT);
+    pinMode(RESET, OUTPUT);
+    pinMode(ROW, OUTPUT);
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+
+
+    digitalWrite(ROW, HIGH);
+    digitalWrite(LATCH, HIGH);
+    digitalWrite(RESET, HIGH);
+}
+
+void loop() {
+    for (int x = 0; x < MATRIX_SIZE; x++)
+        for (int y = 0; y < MATRIX_SIZE; y++) {
+            if (grid[x][y] == 'R') draw(x, y);
+            delay(10);
+        }
+}
+
+void draw(int x, int y) {
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        if (i == x) digitalWrite(ROW, LOW);
+        if (i == y) digitalWrite(RED, HIGH);
+
+        pulse(SHIFT, true);
+
+        digitalWrite(ROW, HIGH);
+        digitalWrite(RED, LOW);
+    }
+    pulse(LATCH, true);
+}
+
+void pulse(int pin, bool reversed) {
+    digitalWrite(pin, !reversed);
+    digitalWrite(pin, reversed);
+}
+
+bool charToArray(char c) {
+    return {false, false}
+}
